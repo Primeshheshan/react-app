fb2b8f14cb2a53dd21b9b019eacede0426ed0d66
primesh <primeshheshan@gmail.com>
2022-05-16 11:51:39 +0530

    change commit place

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,19 @@
-import React, { useState } from "react";
-import { Expenses } from "./components/Expenses/Expenses";
-import { NewExpenses } from "./components/Expenses/NewExpenses/NewExpenses";
-import { IExpense } from "./components/Files/type";
-import "./App.css";
-import { DUMMYEXPENSES } from "./components/Files/DummyExpenses";
+import React, { useState } from 'react';
+import { Expenses } from './components/Expenses/Expenses';
+import { NewExpenses } from './components/Expenses/NewExpenses/NewExpenses';
+import { IExpense } from './components/Files/type';
+import './App.css';
+import { DUMMYEXPENSES } from './components/Files/DummyExpenses';
 
 export const App = () => {
   const [expenses, setExpenses] = useState(DUMMYEXPENSES);
 
+  //addExpenseHandler method
   const addExpenseHandler = (expense: IExpense) => {
     setExpenses((prevExpenses) => {
       return [expense, ...prevExpenses];
     });
-  }; //addExpenseHandler method
+  };
 
   return (
     <div>
